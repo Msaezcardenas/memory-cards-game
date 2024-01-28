bbd725464a8f0d73bcab4fb9dae446b84a503d06
molu <msaezcardenas@gmail.com>
2024-01-28 16:08:48 -0300

    first upload of information

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -11,39 +12,49 @@ const App = () => {
   const [turns, setTurns] = useState(0);
 
   useEffect(() => {
-    fetch(
-      'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=5',
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setCardsInformation(data?.entries);
-      });
-  }, []);
+    if (cardsInformation) {
+      shuffledCards(cardsInformation);
+    } else {
+      fetch(
+        'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=5',
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setCardsInformation(data?.entries);
+        });
+    }
+  }, [cardsInformation]);
 
   const shuffledCards = async () => {
-    const test = await Promise.all(
-      cardsInformation.map(async (card) => {
-        const imagenObject = card?.fields?.image;
-        return imagenObject.url;
-      }),
+    let test = await Promise.all(
+      cardsInformation &&
+        cardsInformation.map(async (card) => {
+          const imagenObject = card?.fields?.image;
+          return imagenObject.url;
+        }),
     );
-
     const cardList = [...test, ...test]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ card, id: Math.random() }));
-
     setCards(cardList);
     setTurns(0);
   };
 
-  cards && console.log(cards, turns);
-
   return (
-    <div className='App'>
+    <div className='App' variant='dark'>
       <h1> Memory Cards Game</h1>
-      <button onClick={shuffledCards}> New game</button>
+      <Button className='btn' onClick={shuffledCards}>
+        {' '}
+        Restart Game
+      </Button>
+      <Container className='mt-4 '>
+        {cards &&
+          cards.map((card) => (
+            <div className='card' key={card.id}>
+              <img src={card.card} />
+            </div>
+          ))}
 
-      <Container>
         <Row>
           <Col sm={8}>sm=8</Col>
           <Col sm={4}>sm=4</Col>
